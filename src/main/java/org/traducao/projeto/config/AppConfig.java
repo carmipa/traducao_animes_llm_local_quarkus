381eb598a65471392e1d24e68e92d04eacb06616
Paulo <PABCARMINATI@GMIL.COM>
2026-07-09 11:05:25 -0300

    fix: remove incompatible EnableConfigurationProperties annotation in AppConfig

--- src/main/java/org/traducao/projeto/config/AppConfig.java
+++ src/main/java/org/traducao/projeto/config/AppConfig.java
@@ -1,20 +1,7 @@
 package org.traducao.projeto.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.traducao.projeto.legendasExtracao.infrastructure.config.ExtratorProperties;
-import org.traducao.projeto.remuxer.infrastructure.config.RemuxerProperties;
-import org.traducao.projeto.telemetria.TelemetriaDatasetProperties;
-import org.traducao.projeto.traducao.infrastructure.config.LlmProperties;
-import org.traducao.projeto.traducao.infrastructure.config.TradutorProperties;
 
 @Configuration
-@EnableConfigurationProperties({
-    LlmProperties.class,
-    TradutorProperties.class,
-    RemuxerProperties.class,
-    ExtratorProperties.class,
-    TelemetriaDatasetProperties.class
-})
 public class AppConfig {
 }
